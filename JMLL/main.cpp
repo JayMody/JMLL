c0343ca969f7d74996ccc89a06cc4b387c2e7741
JayMody <jaymsyndicate@gmail.com>
2019-01-11 18:26:35 -0500

    Initial Commit

---
+++ JMLL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JMLL
+//
+//  Created by Jay Mody on 2019-01-11.
+//  Copyright © 2019 Jay Mody. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
